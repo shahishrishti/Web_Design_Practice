6c4b9558be162d97e084e1f6679f80f5653eb832
Shrishti Shahi <shahishrishti@email.com>
2020-09-19 11:05:27 +0530

    To Do List

---
+++ To Do List/assets/js/script.js
@@ -0,0 +1,25 @@
+//check off todo by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//delete todo
+$("ul").on("click","span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+//create new todo
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todoText + "</li>");
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+})
